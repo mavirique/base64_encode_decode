bae098c283375433fcd50252e05f3e5cb656af77
mavirique <6375502+mavirique@users.noreply.github.com>
2025-10-07 13:18:05 +0800

    fixed build directory

--- build/CMakeFiles/b64_tests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for b64_tests.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/cpp/base64_encode_decode
-# Build directory: /home/user/cpp/base64_encode_decode/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[Base64Tests]=] "/home/user/cpp/base64_encode_decode/build/base64_tests")
-set_tests_properties([=[Base64Tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/user/cpp/base64_encode_decode/CMakeLists.txt;50;add_test;/home/user/cpp/base64_encode_decode/CMakeLists.txt;0;")
-subdirs("_deps/catch2-build")
